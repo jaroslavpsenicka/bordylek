c873172534d099cbc21b2fb2f850fd317cf0b28c
Jaroslav Pšenička (cen84818) <jpsenicka@csas.cz>
2015-08-18 12:40:42 +0200

    update service implemented

---
+++ service/src/main/java/org/bordylek/service/model/Location.java
@@ -0,0 +1,41 @@
+package org.bordylek.service.model;
+
+public class Location {
+
+    private String id;
+    private String name;
+    private float lat;
+    private float lng;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getLat() {
+        return lat;
+    }
+
+    public void setLat(float lat) {
+        this.lat = lat;
+    }
+
+    public float getLng() {
+        return lng;
+    }
+
+    public void setLng(float lng) {
+        this.lng = lng;
+    }
+}
