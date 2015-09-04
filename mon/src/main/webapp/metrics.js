edd240810034b0bceb2d4c1a8f71382558435909
Jaroslav Pšenička (cen84818) <jpsenicka@csas.cz>
2015-09-04 15:56:09 +0200

    new module mon(intoring)

---
+++ mon/src/main/webapp/metrics.js
@@ -0,0 +1,285 @@
+var exampleMetricsData = 
+{
+		  "version" : "3.0.0",
+		  "gauges" : {
+		    "jvm.fd.usage" : {
+		      "value" : "NaN"
+		    },
+		    "jvm.gc.Copy.count" : {
+		      "value" : 42
+		    },
+		    "jvm.gc.Copy.time" : {
+		      "value" : 96
+		    },
+		    "jvm.gc.MarkSweepCompact.count" : {
+		      "value" : 5
+		    },
+		    "jvm.gc.MarkSweepCompact.time" : {
+		      "value" : 278
+		    },
+		    "jvm.memory.heap.committed" : {
+		      "value" : 22478848
+		    },
+		    "jvm.memory.heap.init" : {
+		      "value" : 16777216
+		    },
+		    "jvm.memory.heap.max" : {
+		      "value" : 259522560
+		    },
+		    "jvm.memory.heap.usage" : {
+		      "value" : 0.06344169847892993
+		    },
+		    "jvm.memory.heap.used" : {
+		      "value" : 16464552
+		    },
+		    "jvm.memory.non-heap.committed" : {
+		      "value" : 40370176
+		    },
+		    "jvm.memory.non-heap.init" : {
+		      "value" : 12746752
+		    },
+		    "jvm.memory.non-heap.max" : {
+		      "value" : 167772160
+		    },
+		    "jvm.memory.non-heap.usage" : {
+		      "value" : 0.24022870063781737
+		    },
+		    "jvm.memory.non-heap.used" : {
+		      "value" : 40303688
+		    },
+		    "jvm.memory.pools.Code-Cache.usage" : {
+		      "value" : 0.12493133544921875
+		    },
+		    "jvm.memory.pools.Eden-Space.usage" : {
+		      "value" : 0.08052921556924748
+		    },
+		    "jvm.memory.pools.Perm-Gen.usage" : {
+		      "value" : 0.26905304193496704
+		    },
+		    "jvm.memory.pools.Survivor-Space.usage" : {
+		      "value" : 0.07984655043658088
+		    },
+		    "jvm.memory.pools.Tenured-Gen.usage" : {
+		      "value" : 0.05578599871841816
+		    },
+		    "jvm.memory.total.committed" : {
+		      "value" : 62849024
+		    },
+		    "jvm.memory.total.init" : {
+		      "value" : 29523968
+		    },
+		    "jvm.memory.total.max" : {
+		      "value" : 427294720
+		    },
+		    "jvm.memory.total.used" : {
+		      "value" : 56768240
+		    },
+		    "jvm.thread-states.blocked.count" : {
+		      "value" : 2
+		    },
+		    "jvm.thread-states.count" : {
+		      "value" : 24
+		    },
+		    "jvm.thread-states.daemon.count" : {
+		      "value" : 9
+		    },
+		    "jvm.thread-states.deadlocks" : {
+		      "value" : [ ]
+		    },
+		    "jvm.thread-states.new.count" : {
+		      "value" : 0
+		    },
+		    "jvm.thread-states.runnable.count" : {
+		      "value" : 9
+		    },
+		    "jvm.thread-states.terminated.count" : {
+		      "value" : 0
+		    },
+		    "jvm.thread-states.timed_waiting.count" : {
+		      "value" : 9
+		    },
+		    "jvm.thread-states.waiting.count" : {
+		      "value" : 4
+		    },
+		    "net.sf.ehcache.Cache.cacheName.accuracy" : {
+		      "value" : "None"
+		    },
+		    "net.sf.ehcache.Cache.cacheName.eviction-count" : {
+		      "value" : 0
+		    },
+		    "net.sf.ehcache.Cache.cacheName.hits" : {
+		      "value" : 0
+		    },
+		    "net.sf.ehcache.Cache.cacheName.in-memory-hits" : {
+		      "value" : 0
+		    },
+		    "net.sf.ehcache.Cache.cacheName.in-memory-misses" : {
+		      "value" : 0
+		    },
+		    "net.sf.ehcache.Cache.cacheName.in-memory-objects" : {
+		      "value" : 1
+		    },
+		    "net.sf.ehcache.Cache.cacheName.mean-get-time" : {
+		      "value" : 0.0
+		    },
+		    "net.sf.ehcache.Cache.cacheName.mean-search-time" : {
+		      "value" : 0
+		    },
+		    "net.sf.ehcache.Cache.cacheName.misses" : {
+		      "value" : 0
+		    },
+		    "net.sf.ehcache.Cache.cacheName.objects" : {
+		      "value" : 1
+		    },
+		    "net.sf.ehcache.Cache.cacheName.off-heap-hits" : {
+		      "value" : 0
+		    },
+		    "net.sf.ehcache.Cache.cacheName.off-heap-misses" : {
+		      "value" : 0
+		    },
+		    "net.sf.ehcache.Cache.cacheName.off-heap-objects" : {
+		      "value" : 0
+		    },
+		    "net.sf.ehcache.Cache.cacheName.on-disk-hits" : {
+		      "value" : 0
+		    },
+		    "net.sf.ehcache.Cache.cacheName.on-disk-misses" : {
+		      "value" : 0
+		    },
+		    "net.sf.ehcache.Cache.cacheName.on-disk-objects" : {
+		      "value" : 0
+		    },
+		    "net.sf.ehcache.Cache.cacheName.searches-per-second" : {
+		      "value" : 0
+		    },
+		    "net.sf.ehcache.Cache.cacheName.writer-queue-size" : {
+		      "value" : -1
+		    }
+		  },
+		  "counters" : {
+		    "com.codahale.metrics.servlet.AbstractInstrumentedFilter.activeRequests" : {
+		      "count" : 1
+		    },
+		    "com.codahale.metrics.servlet.AbstractInstrumentedFilter.totalRequests" : {
+			      "count" : 12
+			    }
+		  },
+		  "histograms" : { },
+		  "meters" : {
+		    "com.codahale.metrics.servlet.AbstractInstrumentedFilter.responseCodes.badRequest" : {
+		      "count" : 0,
+		      "m15_rate" : 0.0,
+		      "m1_rate" : 0.0,
+		      "m5_rate" : 0.0,
+		      "mean_rate" : 0.0,
+		      "units" : "events/second"
+		    },
+		    "com.codahale.metrics.servlet.AbstractInstrumentedFilter.responseCodes.created" : {
+		      "count" : 0,
+		      "m15_rate" : 0.0,
+		      "m1_rate" : 0.0,
+		      "m5_rate" : 0.0,
+		      "mean_rate" : 0.0,
+		      "units" : "events/second"
+		    },
+		    "com.codahale.metrics.servlet.AbstractInstrumentedFilter.responseCodes.noContent" : {
+		      "count" : 0,
+		      "m15_rate" : 0.0,
+		      "m1_rate" : 0.0,
+		      "m5_rate" : 0.0,
+		      "mean_rate" : 0.0,
+		      "units" : "events/second"
+		    },
+		    "com.codahale.metrics.servlet.AbstractInstrumentedFilter.responseCodes.notFound" : {
+		      "count" : 0,
+		      "m15_rate" : 0.0,
+		      "m1_rate" : 0.0,
+		      "m5_rate" : 0.0,
+		      "mean_rate" : 0.0,
+		      "units" : "events/second"
+		    },
+		    "com.codahale.metrics.servlet.AbstractInstrumentedFilter.responseCodes.ok" : {
+		      "count" : 12,
+		      "m15_rate" : 0.13835986069336528,
+		      "m1_rate" : 0.011987385129439698,
+		      "m5_rate" : 0.0346026017829465,
+		      "mean_rate" : 0.050450606848816415,
+		      "units" : "events/second"
+		    },
+		    "com.codahale.metrics.servlet.AbstractInstrumentedFilter.responseCodes.other" : {
+		      "count" : 0,
+		      "m15_rate" : 0.0,
+		      "m1_rate" : 0.0,
+		      "m5_rate" : 0.0,
+		      "mean_rate" : 0.0,
+		      "units" : "events/second"
+		    },
+		    "com.codahale.metrics.servlet.AbstractInstrumentedFilter.responseCodes.serverError" : {
+		      "count" : 0,
+		      "m15_rate" : 0.0,
+		      "m1_rate" : 0.0,
+		      "m5_rate" : 0.0,
+		      "mean_rate" : 0.0,
+		      "units" : "events/second"
+		    }
+		  },
+		  "timers" : {
+		    "com.codahale.metrics.servlet.AbstractInstrumentedFilter.requests" : {
+		      "count" : 12,
+		      "max" : 0.7078198490000001,
+		      "mean" : 0.11827764908333334,
+		      "min" : 7.255120000000001E-4,
+		      "p50" : 0.08721737,
+		      "p75" : 0.12283823825000001,
+		      "p95" : 0.7078198490000001,
+		      "p98" : 0.7078198490000001,
+		      "p99" : 0.7078198490000001,
+		      "p999" : 0.7078198490000001,
+		      "stddev" : 0.19549927028488945,
+		      "m15_rate" : 0.13835986069336528,
+		      "m1_rate" : 0.011987385129439698,
+		      "m5_rate" : 0.0346026017829465,
+		      "mean_rate" : 0.05045051601127477,
+		      "duration_units" : "seconds",
+		      "rate_units" : "calls/second"
+		    },
+		    "net.sf.ehcache.Cache.cacheName.gets" : {
+			      "count" : 7,
+			      "max" : 0.18164320000000002,
+			      "mean" : 0.018164320000000002,
+			      "min" : 0.0018164320000000002,
+			      "p50" : 0.018164320000000002,
+			      "p75" : 0.008164320000000002,
+			      "p95" : 0.064320000000002,
+			      "p98" : 0.064320000000002,
+			      "p99" : 0.00864320000000002,
+			      "p999" : 0.00864320000000002,
+			      "stddev" : 0.0,
+			      "m15_rate" : 0.003981468415546746,
+			      "m1_rate" : 0.15403906752017543,
+			      "m5_rate" : 0.09137610702805841,
+			      "mean_rate" : 0.004103965480651415,
+		      "duration_units" : "seconds",
+		      "rate_units" : "calls/second"
+		    },
+		    "net.sf.ehcache.Cache.cacheName.puts" : {
+		      "count" : 1,
+		      "max" : 0.0018164320000000002,
+		      "mean" : 0.0018164320000000002,
+		      "min" : 0.0018164320000000002,
+		      "p50" : 0.0018164320000000002,
+		      "p75" : 0.0018164320000000002,
+		      "p95" : 0.0018164320000000002,
+		      "p98" : 0.0018164320000000002,
+		      "p99" : 0.0018164320000000002,
+		      "p999" : 0.0018164320000000002,
+		      "stddev" : 0.0,
+		      "m15_rate" : 0.003981468415546746,
+		      "m1_rate" : 0.15403906752017543,
+		      "m5_rate" : 0.09137610702805841,
+		      "mean_rate" : 0.004103965480651415,
+		      "duration_units" : "seconds",
+		      "rate_units" : "calls/second"
+		    }
+		  }
+		};
