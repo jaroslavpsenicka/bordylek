0c920b6640466dfb469ec36cea1db88e6e714976
jaroslavpsenicka <kundavesl0>
2015-09-12 08:29:23 +0200

    metric name shown without package, better layout for single-value data

--- mon/src/main/webapp/js/data.js
+++ mon/src/main/webapp/js/data.js
@@ -1,23 +1,34 @@
 app.registerCtrl('DataCtrl', ['$scope', 'metricsService', function ($scope, metricsService) {
 
 	metricsService.meters(function(response) {
-		$scope.meters = response.data;
+		$scope.meters = stripPackage(response.data);
 	});
 
 	metricsService.counters(function(response) {
-		$scope.counters = response.data;
+		$scope.counters = stripPackage(response.data);
 	});
 
 	metricsService.timers(function(response) {
-		$scope.timers = response.data;
+		$scope.timers = stripPackage(response.data);
 	});
 
 	metricsService.gauges(function(response) {
-		$scope.gauges = response.data;
+		$scope.gauges = stripPackage(response.data);
 	});
 
 	metricsService.histograms(function(response) {
-		$scope.histograms = response.data;
+		$scope.histograms = stripPackage(response.data);
 	});
+
+	stripPackage = function(objects) {
+		for (var i = 0; i < objects.length; i++) {
+			var name = objects[i].name;
+			if (name.indexOf('org.bordylek.web.') == 0) {
+				objects[i].name = name.substring('org.bordylek.web.'.length);
+			}
+		}
+
+		return objects;
+	}
 
 }]);
