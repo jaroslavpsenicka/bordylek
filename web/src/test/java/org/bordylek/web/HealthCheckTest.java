178236b6d2493b39eccb497a30015181d39a1876
jaroslavpsenicka <kundavesl0>
2015-09-14 21:10:41 +0200

    healthcheck fix

--- web/src/test/java/org/bordylek/web/HealthCheckTest.java
+++ web/src/test/java/org/bordylek/web/HealthCheckTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -39,6 +40,7 @@ public class HealthCheckTest {
 
     @Before
 	public void before() throws Exception {
+        mongoTemplate.remove(new Query(), "user");
         healthCheckRegistry.register("mongo", new HealthCheckServletContextListener.MongoHealthCheck(mongoTemplate));
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
 	}
