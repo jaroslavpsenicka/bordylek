55c32b90c977bfe98f31074393820ff5d0f3be5b
jaroslavpsenicka <kundavesl0>
2015-09-26 00:32:55 +0200

    #4 Allow timer and metric charts

--- mon/src/main/webapp/js/home.js
+++ mon/src/main/webapp/js/home.js
@@ -7,9 +7,8 @@ app.registerCtrl('HomeCtrl', function ($scope, $routeParams, $modal,
                 enabled: false,
             },
             chart: {
-                zoomType: 'x',
-                width: 250,
-                height: 150
+                type: 'line',
+                zoomType: 'x'
             },
 			credits: {
 				enabled: false
@@ -45,34 +44,59 @@ app.registerCtrl('HomeCtrl', function ($scope, $routeParams, $modal,
 	chartsService.get(function(response) {
 	    for (var i = 0; i < response.length; i++) {
 	        var chartId = response[i].id;
+	        var chartInfo = $scope.decodeName(response[i].serie);
             $scope.charts[chartId] = angular.copy(chartTemplate);
             $scope.charts[chartId].id = chartId;
             $scope.charts[chartId].size = response[i].size || 'col-md-3';
             $scope.charts[chartId].title = {text: response[i].name};
-            if ($scope.charts[chartId].size == 'col-md-6') {
-                $scope.charts[chartId].options.chart.width = 545;
-                $scope.charts[chartId].options.chart.height = 300;
-            } else if ($scope.charts[chartId].size == 'col-md-9') {
-                $scope.charts[chartId].options.chart.width = 835;
-                $scope.charts[chartId].options.chart.height = 450;
-            }
-            $scope.loadChartData('gauge', chartId, response[i].serie, function(id, name, data) {
+            $scope.configureSize($scope.charts[chartId]);
+            $scope.loadChartData(chartId, chartInfo, function(id, name, data) {
                 $scope.charts[id].series = [{name: name, data: data}];
             });
 	    }
 	});
 
-    $scope.loadChartData = function(type, id, name, callback) {
-        metricsService.data({type: type, name: name}, function(response, request) {
+    $scope.loadChartData = function(id, info, callback) {
+        metricsService.data({type: info.type, name: info.name}, function(response, request) {
             var chartData = [];
             for (var j = 0; j < response.data.length; j++) {
-                chartData.push([response.data[j].timestamp, response.data[j].value]);
+                chartData.push([response.data[j].timestamp, response.data[j][info.attr]]);
             }
             if (chartData.length > 0) {
-                callback(id, name, chartData);
+                callback(id, info.name, chartData);
             }
         });
     };
+
+    $scope.decodeName = function(name) {
+        var decoded = {type: 'gauge', name: name, attr: 'value'};
+        if (decoded.name.indexOf(':') > -1) {
+            var parts = decoded.name.split(':');
+            decoded.type = parts[0];
+            decoded.name = parts[1];
+        }
+
+        if (decoded.name.indexOf('/') > -1) {
+            var parts = decoded.name.split('/');
+            decoded.name = parts[0];
+            decoded.attr = parts[1];
+        }
+
+        return decoded;
+    }
+
+    $scope.configureSize = function(chart) {
+        if (chart.size == 'col-md-6') {
+            chart.options.chart.width = 545;
+            chart.options.chart.height = 300;
+        } else if (chart.size == 'col-md-9') {
+            chart.options.chart.width = 835;
+            chart.options.chart.height = 450;
+        } else {
+            chart.options.chart.width = 250;
+            chart.options.chart.height = 150;
+        }
+    }
 
     alertsService.get(function(response) {
         $scope.alerts = response;
